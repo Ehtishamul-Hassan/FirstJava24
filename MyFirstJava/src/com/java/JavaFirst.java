1ba6bbd06d01baf47b47d5ac89878f10db410cd2
Ehtishamul-Hassan <hassanarif282@gmail.com>
2024-03-29 00:24:42 +0530

    First commit

---
+++ MyFirstJava/src/com/java/JavaFirst.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class JavaFirst {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Asif Hassan");
+
+	}
+
+}
--- MyFirstJava/src/module-info.java
+++ MyFirstJava/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module First {
+module MyFirstJava {
 }
