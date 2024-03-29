f59deb459b3b24f3b94656e08ae3123de498e269
Ehtishamul-Hassan <hassanarif282@gmail.com>
2024-03-29 09:06:41 +0530

    second commit

---
+++ SecondJava/src/com/java/Second.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Asif its developers");
+
+	}
+
+}
--- SecondJava/src/module-info.java
+++ SecondJava/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module MyFirstJava {
+module SecondJava {
 }
