aa1520e2891fa399f68ff3712cc6bb3d65ce7b09
Ehtishamul-Hassan <hassanarif282@gmail.com>
2024-03-29 09:13:44 +0530

    change in second

--- SecondJava/src/com/java/Second.java
+++ SecondJava/src/com/java/Second.java
@@ -5,6 +5,7 @@ public class Second {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Asif its developers");
+		System.out.println("killer asif");
 
 	}
 
